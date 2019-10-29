7eede0ea3455b19d73169fd1fcd931959f9b4aeb
Neal Beeken <nbbeeken@gmail.com>
2019-10-29 10:25:53 -0400

    Three

--- toon-shading/src/web/main.ts
+++ toon-shading/src/web/main.ts
@@ -1,6 +1,51 @@
-import { Renderer } from "./render"
+import { gl, resizeCanvas } from './canvas'
+import {
+    Scene,
+    PerspectiveCamera,
+    WebGLRenderer,
+    BoxGeometry,
+    MeshBasicMaterial,
+    Mesh,
+} from "three"
 
 export function main() {
-    const render = new Renderer()
-    render.start()
+    const scene = new Scene()
+    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+    const renderer = new WebGLRenderer({ canvas: gl.canvas, context: gl })
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    const geometry = new BoxGeometry(1, 1, 1)
+    const material = new MeshBasicMaterial({ color: 0x202020 })
+    const cube = new Mesh(geometry, material)
+    scene.add(cube)
+
+    camera.position.z = 5
+
+    function animate() {
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement
+            camera.aspect = canvas.clientWidth / canvas.clientHeight
+            camera.updateProjectionMatrix()
+        }
+
+        cube.rotation.x += 0.01
+        cube.rotation.y += 0.01
+
+        renderer.render(scene, camera)
+        requestAnimationFrame(animate)
+    }
+    animate()
+}
+
+function resizeRendererToDisplaySize(renderer: WebGLRenderer) {
+    const canvas = renderer.domElement
+    const pixelRatio = window.devicePixelRatio
+    const width = canvas.clientWidth * pixelRatio | 0
+    const height = canvas.clientHeight * pixelRatio | 0
+    const needResize = canvas.width !== width || canvas.height !== height
+    if (needResize) {
+        renderer.setSize(width, height, false)
+    }
+    return needResize
 }
